be255213979bfb5a9e5fb6e492440313fa76ed85
JeongHwa <ekdan31@gmail.com>
2024-12-16 16:47:39 +0900

    Feat : ProductNotificationHistory 에 상태 업데이트 메서드 추가

--- src/main/java/com/example/restocknotification/domain/entity/ProductNotificationHistory.java
+++ src/main/java/com/example/restocknotification/domain/entity/ProductNotificationHistory.java
@@ -1,11 +1,14 @@
 package com.example.restocknotification.domain.entity;
 
+import com.example.restocknotification.domain.entity.status.RestockNotificationStatus;
 import com.example.restocknotification.domain.entity.status.StockStatus;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ProductNotificationHistory {
 
@@ -21,21 +24,25 @@ public class ProductNotificationHistory {
 
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
-    private StockStatus stockStatus;
+    private RestockNotificationStatus notificationStatus;
 
     // 마지막 발송 유저 아이디
     @Column(nullable = false)
     private Long userId;
 
-    private ProductNotificationHistory(Product product, Integer stockRound, StockStatus stockStatus, Long userId) {
+    private ProductNotificationHistory(Product product, Integer stockRound, RestockNotificationStatus notificationStatus, Long userId) {
         this.product = product;
         this.stockRound = stockRound;
-        this.stockStatus = stockStatus;
+        this.notificationStatus = notificationStatus;
         this.userId = userId;
     }
 
     // 생성 메서드
-    public static ProductNotificationHistory create(Product product, Integer stockRound, StockStatus stockStatus, Long userId){
-        return new ProductNotificationHistory(product, stockRound, stockStatus, userId);
+    public static ProductNotificationHistory create(Product product, Integer stockRound, RestockNotificationStatus notificationStatus, Long userId){
+        return new ProductNotificationHistory(product, stockRound, notificationStatus, userId);
+    }
+
+    public void updateNotificationStatus(RestockNotificationStatus status){
+        this.notificationStatus = status;
     }
 }
